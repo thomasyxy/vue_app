d1beabb649078c7c666385ef00b74d0c11f20372
yinxueyuan <838476901@qq.com>
2016-11-21 17:19:17 +0800

    require element ui

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import VueRouter from 'vue-router'
 import App from './App'
 import configRoutes from './configs/routes'
+import '../theme/index.css'
 
+Vue.use(ElementUI)
 Vue.use(VueRouter)
 
 const router = new VueRouter({
