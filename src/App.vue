42881b4d4a43fb537125a94a86fc58665cdb4115
yinxueyuan <wb-yxy230225@alibaba-inc.com>
2016-12-02 10:37:42 +0800

    add jsonp request

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,19 @@
 import Navigator from './components/Navigator'
 
 export default {
+  data () {
+    return {}
+  },
+  mounted () {
+    this.$http.jsonp('http://www.yinxueyuan.com/vueapp/jsonp?callback=hello', {}, {
+      headers: {},
+      emulateJSON: true
+    }).then(function (res) {
+      this.initData = res.data
+    }, function (res) {
+      console.log(res)
+    })
+  },
   components: {
     Navigator
   }
