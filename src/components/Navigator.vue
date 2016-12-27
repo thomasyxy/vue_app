d81e3fe794b04b084d556c5221c92db7a9e8c997
yinxueyuan <838476901@qq.com>
2016-12-27 18:42:56 +0800

    添加路由

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -2,13 +2,14 @@
   div#focus-ease-navigator.main-nav
     el-menu(default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect")
       el-menu-item(index="1")
-        router-link(:to="{ name: '#' }") 处理中心
+        router-link(to="/home") 处理中心
       el-submenu(index="2")
         template(slot="title") 我的工作台
         el-menu-item(index="2-1") 选项1
         el-menu-item(index="2-2") 选项2
         el-menu-item(index="2-3") 选项3
-      el-menu-item(index="3") 订单管理
+      el-menu-item(index="3")
+        router-link(to="/yixuan") 订单管理
 </template>
 
 <script>
